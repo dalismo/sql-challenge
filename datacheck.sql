09b2f8244de492504b821c050a426289fba5a62d
Alberto Gonzalez <albertogonzalez@Albertos-MacBook-Air.local>
2021-06-04 14:30:51 -0400

    working on 1st query

---
+++ datacheck.sql
@@ -0,0 +1,16 @@
+select * from "Departments";
+
+select * from "Dept_Emp";
+
+select * from "Dept_Manager";
+
+select * from "Employees";
+
+select * from "Salaries";
+
+select * from "Titles";
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+select emp_no, last_name, first_name, sex
+from "Employees";
+
