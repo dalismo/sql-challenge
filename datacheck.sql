c2cffe587be40119268ed00a3f41823dbf4ac250
Alberto Gonzalez <albertogonzalez@Albertos-MacBook-Air.local>
2021-06-04 14:55:36 -0400

    question 1 finished

--- datacheck.sql
+++ datacheck.sql
@@ -11,6 +11,8 @@ select * from "Salaries";
 select * from "Titles";
 
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
-select emp_no, last_name, first_name, sex
-from "Employees";
+select "Employees".emp_no, last_name, first_name, sex, salary
+from "Employees"
+left join "Salaries" on "Employees".emp_no = "Salaries".emp_no
+;
 
