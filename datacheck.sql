b97fdbc04bb8934b9689727545d3e3bd84a24325
Alberto Gonzalez <albertogonzalez@Albertos-MacBook-Air.local>
2021-06-04 17:17:53 -0400

    fisnished question 5 sql

--- datacheck.sql
+++ datacheck.sql
@@ -37,7 +37,8 @@ join "Departments" on "Dept_Emp".dept_no = "Departments".dept_no
 ;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
-
+select first_name, last_name, sex 
+from "Employees"
+where first_name = 'Hercules' and last_name like 'B%';
 
 
